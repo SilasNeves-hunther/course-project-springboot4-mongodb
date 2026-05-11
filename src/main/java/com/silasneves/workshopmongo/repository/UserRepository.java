7772a357f5a0d7ad5ec9daf87c3df2d0863ef545
Silas Neves <silashunther1@gmail.com>
2026-05-11 10:54:48 -0400

    Operação de instanciação da base de dados

--- src/main/java/com/silasneves/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/silasneves/workshopmongo/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.silasneves.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.silasneves.workshopmongo.domain.User;
 
-
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
 }
