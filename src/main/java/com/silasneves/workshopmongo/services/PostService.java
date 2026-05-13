2d7c0b997ae46877c492dca740f1f02538291bdd
Silas Neves <silashunther1@gmail.com>
2026-05-13 09:51:14 -0400

    Obtendo um post por id

---
+++ src/main/java/com/silasneves/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.silasneves.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silasneves.workshopmongo.domain.Post;
+import com.silasneves.workshopmongo.repository.PostRepository;
+import com.silasneves.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+
+}
