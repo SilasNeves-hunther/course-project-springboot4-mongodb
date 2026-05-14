afe775a4ea0620801a83b911b4f4232098adaeb5
Silas Neves <silashunther1@gmail.com>
2026-05-14 08:56:18 -0400

    Consulta simples com query methods

--- src/main/java/com/silasneves/workshopmongo/services/PostService.java
+++ src/main/java/com/silasneves/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.silasneves.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
